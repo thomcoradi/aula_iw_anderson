d71681eb0a5d74fe766eedeccf1637de275bf496
thomcoradi <125597694+thomcoradi@users.noreply.github.com>
2024-03-04 17:05:06 -0300

    aula_03 IW2

---
+++ aula03/banco.js
@@ -0,0 +1,13 @@
+function dados(){
+
+    const ds = {
+                    {id:1,login:"Thom",senha:"12345",email:"tc@gmail.com"}
+                    {id:2,login:"Thomas",senha:"15156",email:"tec@gmail.com"}
+                    {id:3,login:"Thom C",senha:"55555",email:"tech@gmail.com"}
+                }
+
+}
+
+    const dados = JSON.stringify(ds)
+    
+    localStorage.setItem = ("nome", dados)
